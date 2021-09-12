738a175d63104aa1db983ef7f5743bff65db2941
tbenjamin1 <tbenpollyl@gmail.com>
2021-09-12 13:50:33 +0300

    form validation

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,16 +1,20 @@
 import { Typography } from "@material-ui/core";
 import React from "react";
-import './Profile.css';
+import "./Profile.css";
 
-const Profile = () => {
+
+const Profile = (props) => {
+
+
   return (
+    
     <div className="profile container_shadow">
       <div className="profile_name">
         <Typography className="name">TUYISINGIZE BENJAMIN</Typography>
         <Typography className="title">web developer</Typography>
       </div>
-      <figure className="profile_image" >
-        <img src={require('../../assets/images/pp.jpg').default} alt="" />
+      <figure className="profile_image">
+        <img src={require("../../assets/images/pp.jpg").default} alt="" />
       </figure>
       <div className="profile_information">
         insert timeline
@@ -18,7 +22,9 @@ const Profile = () => {
         <button>my cv</button>
       </div>
     </div>
-  );
+
+);
 };
+
 
 export default Profile;
